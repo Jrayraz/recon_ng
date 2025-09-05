8c9274a239ae7b9d0fe438c19ce0b502bc1eaa6b
Jonathan Rosenbum <jonathan.rosenbum@shitzoid-software.com>
2025-09-05 05:39:16 +0000

    Add files via upload

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-rm -r sub.txt takeovers.txt /home/shared/logs/recon_ap.log recon_ng.log recon_ng_nmap.log recon_ng_curl.log recon_ap.log recon_ng_nmap.log recon_ap_nmap.log recon_ap_curl.log recon_ap_takeover.log
+rm -r sub.txt takeovers.txt /home/shared/logs/recon_ap.log recon_ng.log recon_ng_nmap.log recon_ng_curl.log recon_ap.log recon_ng_nmap.log recon_ap_nmap.log recon_ap_curl.log recon_ap_takeover.log recon_ng_curl.log.txt recon_ng_curl.log recon_ng.log.txt recon_ng_extended.log recon_ng_extended.log.txt recon_ng_nmap.log recon_ng_nmap.log.txt recon_ng_takeover.log recon_ng_takeover.log.txt recon_ng_dns.log
+rm -rf extended_enum_*/
+
